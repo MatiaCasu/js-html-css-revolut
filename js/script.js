c368c300b1d761395dce0796c71022f8359f34d5
Matia <matia.casu@gmail.com>
2021-03-15 17:11:18 +0100

    Visualizzazione menu al click e hover in js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+// $(function(){
+//
+//   var buttonList = $("li.li_list");
+//
+//   buttonList.hover(function(){
+//     $(this).children(".hidden_list").fadeToggle();
+//   });
+// });
 
+$(function(){
+
+  var buttonList = $("li.li_list");
+
+  buttonList.click(function(){
+    $(this).children(".hidden_list").slideToggle();
+  });
+});
